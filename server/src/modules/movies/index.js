67548e888214a24c4ba9d8d4f8c4926956a5023a
Nikita Musaiev <n.musaiev@gmail.com>
2023-02-15 22:07:09 +0200

    changed URLSearchparams generation on server side

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -1,21 +1,59 @@
 const axios = require('axios')
 const {Movies} = require("./entities/Movies");
-const { API_KEY, API_BASE_URL } = require('../../config');
+const {API_KEY, API_BASE_URL} = require('../../config');
 
-const getPopularMovies = async (page=1, language = "en-US") => {
-    const result = await axios.get(`${API_BASE_URL}movie/popular?api_key=${API_KEY}&language=${language}&page=${page}`)
+const getPopularMovies = async (page = 1, language = "en-US") => {
+
+    const searchParams = new URLSearchParams({
+        api_key: API_KEY,
+        language: language,
+        page: page,
+    })
+
+    const result = await axios.get(`${API_BASE_URL}/movie/popular?${searchParams}`)
 
     return new Movies(result.data)
 }
 
 const getDetails = (id, language = "en-US") => {
+    const searchParams = ({
+        api_key: API_KEY,
+        language: language,
+    })
+    return axios.get(`${API_BASE_URL}/movie/${id}?${searchParams}`)
+}
 
-    return axios.get(`${API_BASE_URL}movie/${id}?api_key=${API_KEY}&language=${language}`)
+// TODO: ID:112
+const getByFilters = async (language = "en-US", sort_by, year, with_genres, without_genres, page = 1) => {
 
+    const searchParams = new URLSearchParams({
+        api_key: API_KEY  ,
+        language: language,
+        page: page,
+        sort_by: sort_by,
+        with_genres: with_genres,
+        without_genres: without_genres,
+    });
 
+    const result = await axios.get(`${API_BASE_URL}/discover/movie?${searchParams.toString()}`)
+    return new Movies(result.data)
+}
+
+const getGenres = async (language = "en-US") => {
+
+    const searchParams = new URLSearchParams({
+        api_key: API_KEY  ,
+        language: language
+        });
+
+    const result = await axios.get(`${API_BASE_URL}/genre/movie/list?${searchParams.toString()}`);
+
+    return result.data.genres
 }
 
 module.exports = {
     getPopularMovies,
-    getDetails
+    getDetails,
+    getByFilters,
+    getGenres
 }
