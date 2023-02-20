2a1d2091ed7a3f48d700a05cd8fa6e291e922519
Nikita Musaiev <n.musaiev@gmail.com>
2023-02-20 15:28:33 +0200

    610: i18n support for all app

--- client/src/const.js
+++ client/src/const.js
@@ -1,14 +1,16 @@
+import {FormattedMessage} from "react-intl";
+
 export const MAX_SELECTED_MOVIES = 20;
 export const CONFIRM_TIMEOUT = 1500;
 export const STORAGE_KEY_LOCALE = 'locale';
 export const STORAGE_KEY_THEME = 'theme';
 
 export const SORT_OPTIONS = [
-    {name: "Popularity", value: "popularity"},
-    {name: "Release Date", value: "release_date"},
-    {name: "Vote", value: "vote_average"},
-    {name: "Title", value: "original_title"},
-    {name: "Release Date", value: "primary_release_date"}
+    {name: <FormattedMessage id="filters.sort.popularity"/>, value: "popularity"},
+    {name: <FormattedMessage id="filters.sort.release_date"/>, value: "release_date"},
+    {name: <FormattedMessage id="filters.sort.vote_average"/>, value: "vote_average"},
+    {name: <FormattedMessage id="filters.sort.original_title"/>, value: "original_title"},
+    {name: <FormattedMessage id="filters.sort.primary_release_date"/>, value: "primary_release_date"}
 ]
 
 export const SORT_DIRECTION = {
